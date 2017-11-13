11f217f576420a14c8946f750bc915a46ea8b895
ZhuLige <zhulige@qq.com>
2017-11-13 15:33:03 +0800

    CustomVision_Prediction_Sample

---
+++ CustomVision_Prediction_Sample/LogHelper.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CustomVision_Prediction_Sample
+{
+    public class LogHelper
+    {
+
+        private const string _logFormat = "{0:T}; Key; {1}; Detail; {2}";
+
+        /// <summary>
+        /// 写日志
+        /// </summary>
+        /// <param name="level"></param>
+        /// <param name="msg"></param>
+        /// <param name="msgKey">自定义msgKey</param>
+        /// <param name="moduleName">自定义模块</param>
+        public static void WriteLine(LogLevel level ,string msg, string msgKey = null, string moduleName = "")
+        {
+            string w = DateTime.Now.ToString() + " " + msg + " " + msgKey;
+            switch (level)
+            {
+                case LogLevel.Trace:
+                    System.Console.WriteLine(w);
+                    WriteLog(new LogInfo(level, msg, msgKey, moduleName));
+                    break;
+                case LogLevel.Info:
+                    System.Console.WriteLine(w);
+                    WriteLog(new LogInfo(level, msg, msgKey, moduleName));
+                    break;
+                case LogLevel.Warning:
+                    System.Console.WriteLine(w);
+                    WriteLog(new LogInfo(level, msg, msgKey, moduleName));
+                    break;
+                case LogLevel.Error:
+                    System.Console.WriteLine(w);
+                    WriteLog(new LogInfo(level, msg, msgKey, moduleName));
+                    break;
+
+            }
+           
+        }
+
+        /// <summary>
+        /// 写日志
+        /// </summary>
+        /// <param name="level"></param>
+        /// <param name="msgObj">Object类型</param>
+        /// <param name="msgKey">自定义msgKey</param>
+        /// <param name="moduleName">自定义模块</param>
+        public static void WriteLine(LogLevel level, object msgObj,string msgKey = null, string moduleName = "")
+        {
+            string w = DateTime.Now.ToString() + " " + Newtonsoft.Json.JsonConvert.SerializeObject(msgObj);
+            System.Console.WriteLine(w);
+            WriteLog(new LogInfo(level, msgObj, msgKey, moduleName));
+        }
+
+
+        private static string getLogFilePath(string module, LogLevel level)
+        {
+            string _logBaseDirPath = "logs";
+            string dirPath = string.Format(@"{0}/{1}/{2}/", _logBaseDirPath, module, level);
+            if (!Directory.Exists(dirPath))
+                Directory.CreateDirectory(dirPath);
+            return string.Concat(dirPath, DateTime.Now.ToString("yyyyMMddHH"), ".log");
+        }
+
+        private static object obj = new object();
+
+        /// <summary>
+        /// 写日志
+        /// </summary>
+        /// <param name="info"></param>
+        private static void WriteLog(LogInfo info)
+        {
+            lock (obj)
+            {
+                string filePath = getLogFilePath(info.ModuleName, info.Level);
+
+                using (StreamWriter sw = new StreamWriter(new FileStream(filePath, FileMode.Append, FileAccess.Write), Encoding.UTF8))
+                {
+                    sw.WriteLine(format: _logFormat, arg0: DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), arg1: info.MsgKey, arg2: info.Msg);
+
+                }
+            }
+
+        }
+
+
+    }
+
+
+    /// <summary>
+    /// 日志实体
+    /// </summary>
+    public sealed class LogInfo
+    {
+        /// <summary>
+        /// 空构造函数
+        /// </summary>
+        public LogInfo()
+        {
+        }
+
+        /// <summary>
+        /// 日志构造函数
+        /// </summary>
+        /// <param name="loglevel"></param>
+        /// <param name="logMsg"></param>
+        /// <param name="msgKey"></param>
+        /// <param name="moduleName"></param>
+        internal LogInfo(LogLevel loglevel, object logMsg, string msgKey = null, string moduleName = "")
+        {
+            Level = loglevel;
+            ModuleName = moduleName;
+            this.Msg = logMsg;
+            MsgKey = msgKey;
+        }
+
+        /// <summary>
+        /// 日志等级
+        /// </summary>
+        public LogLevel Level { get; set; }
+
+        /// <summary>
+        /// 日志类型
+        /// </summary>
+        public string ModuleName { get; set; }
+
+        /// <summary>
+        ///   key值  可以是自定义的标识  
+        ///   根据此字段可以处理当前module下不同复杂日志信息
+        /// </summary>
+        public string MsgKey { get; set; }
+
+        /// <summary>
+        /// 日志信息  可以是复杂类型  如 具体实体类
+        /// </summary>
+        public object Msg { get; set; }
+
+        /// <summary>
+        /// 编号（全局唯一）
+        /// </summary>
+        public string LogCode { get; set; }
+    }
+
+    /// <summary>
+    /// 日志等级
+    /// </summary>
+    public enum LogLevel
+    {
+        /// <summary>
+        /// 跟踪查看
+        /// </summary>
+        Trace = 0,
+        /// <summary>
+        /// 信息
+        /// </summary>
+        Info = 1,
+
+        /// <summary>
+        /// 警告
+        /// </summary>
+        Warning = 2,
+
+        /// <summary>
+        /// 错误
+        /// </summary>
+        Error = 3,
+
+    }
+
+
+}
---
+++ CustomVision_Prediction_Sample/Program.cs
@@ -0,0 +1,127 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CustomVision_Prediction_Sample
+{
+    class Program
+    {
+        static string key;
+        static string url;
+        static HttpClient client = null;
+
+        static void Main(string[] args)
+        {
+            if (args.Length >= 2)
+            {
+                key = args[0];
+                Console.Write("key: " + key);
+                url = args[1];
+                Console.Write("url: " + url);
+            }
+            else
+            {
+                Console.Write("Enter key: ");
+                key = Console.ReadLine();
+                Console.Write("Enter url: ");
+                url = Console.ReadLine();
+            }
+
+            Console.Write("Enter image file path: ");
+
+            string imageFilePath = Console.ReadLine();
+
+            List<V_KeyValue> _List = GetImageList(imageFilePath);
+
+            int i = 0;
+            while (i < _List.Count)
+            {
+                MakePredictionRequest(imageFilePath + "/" + _List[i].Value).Wait();
+                i++;
+                Thread.Sleep(200);
+            }
+
+            Console.WriteLine("\n\n\nHit ENTER to exit...");
+
+            Console.ReadLine();
+        }
+
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+        
+        static async Task MakePredictionRequest(string imageFilePath)
+        {
+            try
+            {
+                if (client == null)
+                {
+                    client = new HttpClient();
+
+                    // Request headers - replace this example key with your valid subscription key.
+                    client.DefaultRequestHeaders.Add("Prediction-Key", key);
+                }
+
+                HttpResponseMessage response;
+
+                // Request body. Try this sample with a locally stored image.
+                byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+                using (var content = new ByteArrayContent(byteData))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                    response = await client.PostAsync(url, content);
+                    Hashtable _Hashtable = JsonConvert.DeserializeObject<Hashtable>(await response.Content.ReadAsStringAsync());
+                    List<Hashtable> _List = JsonConvert.DeserializeObject<List<Hashtable>>(System.Convert.ToString(_Hashtable["Predictions"]));
+                    string str = string.Empty;
+                    for (int i = 0; i < _List.Count; i++)
+                    {
+                        str += "Tag_" + _List[i]["Tag"] + "; " + System.Convert.ToInt32(_List[i]["Probability"]) * 100 + "%; ";
+                    }
+                    LogHelper.WriteLine(LogLevel.Trace, str, imageFilePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.WriteLine(LogLevel.Error, ex.Message, imageFilePath);
+            }
+        }
+
+        /// <summary>
+        /// 获取目录内图片列表
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        static List<V_KeyValue> GetImageList(string file)
+        {
+            List<V_KeyValue> _List = new List<V_KeyValue>();
+
+            DirectoryInfo folder = new DirectoryInfo(file);
+
+            foreach (FileInfo _FileInfo in folder.GetFiles("*.png"))
+            {
+                V_KeyValue _V_KeyValue = new V_KeyValue();
+                _V_KeyValue.Key = _FileInfo.Name.Replace(".png", "");
+                _V_KeyValue.Value = _FileInfo.Name;
+                _List.Add(_V_KeyValue);
+            }
+
+            return _List;
+        }
+    }
+
+    public class V_KeyValue
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
